2efe85b5c2cbb30ec5836c3009dd8c2e785b82ef
angelicapages <angelicapages@gmail.com>
2020-08-28 16:47:36 -0300

    Me estás oyendo inútil? Aquí está tu código- Paquita la del barrio

---
+++ index.js
@@ -0,0 +1,89 @@
+//EJERCICIO 1
+
+//const saludo = prompt("Dime tu nombre por favor")
+
+//const titulo = document.getElementById("titulo")
+
+//titulo.textContent = "Hola" + " " + (saludo)
+
+
+
+
+//EJERCICIO 2
+
+//const color = prompt ("Dime tu color favorito en su sistema hexadecimal")
+
+//const body= document.body
+//body.style.backgroundColor = (color)
+
+
+
+
+//EJERCICIO 3
+
+//const rgb1 = prompt("Dime un número")
+//const rgb2 = prompt("Dime otro número")
+//const rgb3 = prompt("Dime un último número")
+
+//const rgb = "rgb("+ rgb1 + "," + rgb2 + "," + rgb3 + ")"
+
+//console.log(rgb)
+//const body = document.body
+//body.style.backgroundColor = rgb
+
+
+
+
+//EJERCICIO IMAGEN
+
+//const imagen = document.getElementById("imagen")
+//const preguntaTamano = prompt("Dime un tamaño y cambia el tamaño de Paquita, ¿Chica mediana o grande?")
+
+
+//if (preguntaTamano == "chica") {
+  //  imagen.classList.add("imagen1")
+//}
+//else if (preguntaTamano == "mediana") {
+  //  imagen.classList.add("imagen2")
+//}
+//else if (preguntaTamano == "grande") {
+  //  imagen.classList.add("imagen3")
+//}
+//else {
+    //alert("me estás oyendo inútil? Dime un tamaño")
+//}
+
+
+
+
+
+//EJERCICIO TEMPERATURA
+
+//const temperatura = prompt("En qué temperatura estamos?")
+
+//if (temperatura < 0) {
+  //  titulo.classList.add("color-azul") 
+    //}
+    //else if (temperatura >= 0 && temperatura < 15) {
+    //titulo.classList.add("color-celeste")
+    //}
+    //else if (temperatura >= 15 && temperatura < 25) {
+    //titulo.classList.add("color-verde")
+    //}
+    //else if (temperatura >=25 && temperatura < 30) {
+    //titulo.classList.add("color-amarillo")
+    //}
+    //else if (temperatura >= 30 && temperatura < 35) {
+      //  titulo.classList.add("color-naranja")
+    //}
+    //else if (temperatura <= 35) {
+      //  titulo.classList.add("color-rojo")
+    //}
+    //else {
+      //  alert ("Por favor, introduce un valor numérico")
+    //}
+
+
+
+    
+//EJERCICIO PROGRESO 
